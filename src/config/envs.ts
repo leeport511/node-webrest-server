5ce96aa2f8f923d55b7284b77bb59fe94491e83b
Leonardo Portillo <leonardoandres.portillo@gmail.com>
2024-05-14 20:18:20 -0300

    commit

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,7 +4,7 @@ import { get } from 'env-var'
 export const envs = {
     PORT: get('PORT').required().asPortNumber(),
     PUBLIC_PATH: get('PUBLIC_PATH').default('public').asString(),
-    // POSTGRES_URL: get('POSTGRES_URL').required().asString(),
+    POSTGRES_URL: get('POSTGRES_URL').default('public').asString(),
     // POSTGRES_USER: get('POSTGRES_USER').required().asString(),
     // POSTGRES_DB: get('POSTGRES_DB').required().asString(),
     // POSTGRES_PORT: get('POSTGRES_PORT').required().asPortNumber(),
