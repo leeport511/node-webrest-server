6741792e8e2d62154cde5181d9453821a85f2626
Leonardo Portillo <leonardoandres.portillo@gmail.com>
2024-05-14 20:10:50 -0300

    changue postgres_url required stat in envs.ts

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,10 +4,10 @@ import { get } from 'env-var'
 export const envs = {
     PORT: get('PORT').required().asPortNumber(),
     PUBLIC_PATH: get('PUBLIC_PATH').default('public').asString(),
-    POSTGRES_URL: get('POSTGRES_URL').required().asString(),
-    POSTGRES_USER: get('POSTGRES_USER').required().asString(),
-    POSTGRES_DB: get('POSTGRES_DB').required().asString(),
-    POSTGRES_PORT: get('POSTGRES_PORT').required().asPortNumber(),
-    POSTGRES_PASSWORD: get('POSTGRES_PASSWORD').required().asIntPositive(),
-    NODE_ENV: get('NODE_ENV').required().asString(),
+    // POSTGRES_URL: get('POSTGRES_URL').required().asString(),
+    // POSTGRES_USER: get('POSTGRES_USER').required().asString(),
+    // POSTGRES_DB: get('POSTGRES_DB').required().asString(),
+    // POSTGRES_PORT: get('POSTGRES_PORT').required().asPortNumber(),
+    // POSTGRES_PASSWORD: get('POSTGRES_PASSWORD').required().asIntPositive(),
+    // NODE_ENV: get('NODE_ENV').required().asString(),
 }
